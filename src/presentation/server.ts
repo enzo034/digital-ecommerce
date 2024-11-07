38d78f3b2fe2991762142e2f78dea978aab1f02c
enzo034 <enzo03453@gmail.com>
2024-11-07 17:09:35 -0300

    feat(cors): Cors added in the server.

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import cors from 'cors'
 
 interface Options {
   port: number;
@@ -28,6 +29,8 @@ export class Server {
     //* Middlewares
     this.app.use( express.json() ); // raw
     this.app.use( express.urlencoded({ extended: true }) ); // x-www-form-urlencoded
+
+    this.app.use(cors());
 
     //* Routes
     this.app.use( this.routes );
